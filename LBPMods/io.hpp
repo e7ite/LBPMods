e0a00d6d11f5e39c67e3b25914984cca0f122d02
E7ite <z7z.e7ite.z7z@gmail.com>
2024-02-05 01:03:09 -0800

    Update comment to game's buffered print API to hopefully to make it more clear.
    The previous message was not very clear since there wasn't an obvious "file" being used with the API. I think it's easier to figure out what the output string is with this API.

--- LBPMods/io.hpp
+++ LBPMods/io.hpp
@@ -4,8 +4,8 @@
 
 namespace lbp {
 
-// Typical printf implementation in game, but that doesn't print until newline
-// is submitted in file. No big use for this besides sanity check.
+// Typical printf implementation in game, but doesn't print until newline
+// character is present in output string after formatting.
 extern libpsutil::symbol<void(const char *, ...)> buffered_printf;
 
 }  // namespace lbp
